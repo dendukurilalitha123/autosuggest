5303af02d42e618e17e2713aeb89eed24bcfe68a
dendukurilalitha123 <dendukurilalitha123@gmail.com>
2026-06-19 14:31:18 +0530

    third commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 6700;
+
+app.use(express.static("Frontend"));
+app.listen(port, function() {
+    console.log('App is running on http://localhost:' + port);
+});
