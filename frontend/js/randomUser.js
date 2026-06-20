314437e758dbcb1aa34bce23e52568a6a681e759
dendukurilalitha123 <dendukurilalitha123@gmail.com>
2026-06-20 10:35:05 +0530

    randomUSer

---
+++ frontend/js/randomUser.js
@@ -0,0 +1,19 @@
+function randomUser() {
+    fetch('https://randomuser.me/api/')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var user = data.results[0];
+            var fullName = user.name.title + ' ' + user.name.first + ' ' + user.name.last;
+            document.getElementById("user-img").src = user.picture.large;
+            // name 
+            document.getElementById("user-name").innerText = fullName;
+            //gender
+            document.getElementById("user-gender").innerText = user.gender;
+        })
+        .catch(function (error) {
+            console.log('Error:', error);
+        });
+
+    }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 6700;
 
-app.use(express.static("Frontend"));
+app.use(express.static("frontend"));
 app.listen(port, function() {
     console.log('App is running on http://localhost:' + port);
 });
